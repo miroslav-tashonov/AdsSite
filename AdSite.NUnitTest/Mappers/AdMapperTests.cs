5807a332e7ea9b125e89303d27bced6a8c724542
Miroslav Tashonov <miki@miki-PC>
2019-10-14 20:40:34 +0200

    nunit test init

---
+++ AdSite.NUnitTest/Mappers/AdMapperTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using AdSite.Models.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdSite.Models.Mappers.Tests
+{
+    [TestFixture()]
+    public class AdMapperTests
+    {
+        [Test()]
+        public void MapAdFromAdEditModelTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
